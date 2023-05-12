c19094f7522112963f89849c5035b5e240f8447a
Adeyemi Olusegun <segunisaboy@gmail.com>
2023-05-12 16:10:31 +0100

    first commit

---
+++ Desktop/tail/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        baba: "#00ff00",
+      },
+      fontFamily: {
+        poppins: ["poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
